374817e4ff22c1c598a7f1f41c996b150e61f362
romich17 <rohar77@gmail.com>
2018-02-16 12:46:59 +0300

    Add files via upload

---
+++ admin/language/russian/module/digipost.php
@@ -0,0 +1,69 @@
+<?php
+// Heading
+$_['heading_title']             = 'api.Digi-Post.ru - плагин для комфортной работы с Почтой России';
+
+// Text
+$_['text_module']               = 'Модули';
+$_['text_success']              = 'Успех: Вы изменили настройки!';
+$_['text_ext_version']          = 'Версия модуля:';
+$_['text_ext_name']             = 'Название Модуля:';
+$_['text_ext_compat']           = 'Совместимость:';
+$_['text_ext_support']          = 'Поддержка:';
+$_['text_ext_url']              = 'URL модуля:';
+$_['text_ext_legal']            = 'Информация правообладателя:';
+$_['text_ext_subject']          = '%s support needed';
+$_['text_terms']                = 'Лицензия';
+$_['text_help_request']         = 'Запросить поддержку';
+$_['text_asking_help']          = 'На что стоит обратить внимание, если вы собрались запросить поддержку';
+$_['text_forum']                = 'Форум';
+$_['text_from_name']        = '<span class="help" style="display:inline">ИП Леванов Борис Викторович или ООО "КофеХаус"</span>';
+$_['text_from_address_1']        = '<span class="help" style="display:inline">ул. Красная 5 оф. 7, Краснодар</span>';
+$_['text_from_address_2']        = '<span class="help" style="display:inline">Краснодарский край, Россия</span>';
+$_['text_postindex']        = '<span class="help" style="display:inline">350000</span>';
+$_['text_doc_type']        = '<span class="help" style="display:inline">паспорт</span>';
+$_['text_doc_serial']        = '<span class="help" style="display:inline">2005</span>';
+$_['text_doc_num']        = '<span class="help" style="display:inline">412312</span>';
+$_['text_doc_date']        = '<span class="help" style="display:inline">02.06.2005</span>';
+$_['text_doc_received_by']        = '<span class="help" style="display:inline">1м отделом милиции УВД города Краснодара</span>';
+
+
+$_['text_yes']               = 'Да';
+$_['text_no']               = 'Нет';
+
+$_['text_disabled']               = 'Выключен';
+$_['text_enabled']               = 'Включен';
+
+// Tab
+$_['tab_about']                 = 'О модуле';
+
+// Entry
+$_['entry_status']              = 'Статус:';
+$_['entry_name']       = 'ФИО отправителя:';
+$_['entry_address_1']         = 'Адрес 1:';
+$_['entry_address_2']     = 'Адрес 2:';
+$_['entry_postindex']            = 'Индекс места отправления';
+
+$_['entry_doc_type']              = 'Тип документа:';
+$_['entry_doc_serial']       = 'Серия:';
+$_['entry_doc_num']         = 'Номер:';
+$_['entry_doc_date']     = 'Дата выдачи:';
+$_['entry_doc_received_by']            = 'Выдан:';
+//юр лицо
+$_['entry_inn']              = 'ИНН:';
+$_['entry_bik']       = 'БИК:';
+$_['entry_correspondent_account']         = 'Кор. счет:';
+$_['entry_current_account']     = 'Расчетный счет:';
+$_['entry_bank_name']            = 'Наименование банка:';
+
+$_['text_rpb_inn']              = 'ИНН:';
+$_['text_bik']       = 'БИК:';
+$_['text_correspondent_account']         = 'Кор. счет:';
+$_['text_current_account']     = 'Расчетный счет:';
+$_['text_bank_name']            = 'Наименование банка:';
+
+// Error
+$_['error_permission']          = 'Ошибка: У вас нет прав на изменение модуля!';
+$_['error_vqmod']               = 'Похоже что vQmod не установлен, <a target="_blank" href="https://github.com/vqmod/vqmod">скачайте его здесь!</a>';
+$_['error_unsaved_settings']    = 'Имеются несохраненные настройки! Пожалуйста сохраните настройки.';
+
+?>
---
+++ admin/language/russian/shipping/digicalc.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']      = 'Почта России + EMS';
+
+// Text
+$_['text_shipping']      = 'Доставка';
+$_['text_success']       = 'Успех: Вы успешно изменили параметры!';
+$_['text_enabled']      = 'Включен';
+$_['text_disabled']      = 'Выключен';
+$_['text_yes']      = 'Да';
+$_['text_no']      = 'Нет';
+
+
+// Entry
+$_['entry_postcode']     = 'Индекс:<span class="help">Укажите индекс почтового отделения места отправки.</span>';
+$_['entry_display_time'] = 'Показывать сроки доставки:<br /><span class="help">Хотите показывать клиенту сроки доставки? (например от 3 до 5 дней)</span>';
+$_['entry_weight_class'] = 'Тип веса:<span class="help">Установите в граммы!</span>';
+$_['entry_geo_zone']     = 'Гео зона:';
+$_['entry_status']       = 'Статус:';
+$_['entry_sort_order']   = 'Порядок сортировки:';
+
+// Error
+$_['error_permission']   = 'Внимание: У вас нет прав на редактирование этого модуля!';
+$_['error_postcode']     = 'Индекс должен быть шестизначным!';
+?>
